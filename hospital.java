ee92b493385b7b80ea2bea1ef24f6420e50c727f
madhuk54 <krishnashetty98765@gmail.com>
2025-04-29 18:41:20 +0530

    added

---
+++ hospital.java
@@ -0,0 +1,18 @@
+package practice1;
+
+public class hospital {
+    public static void main(String[] args) {
+        String patientId = "P12345"; 
+        String upiId = "patient@upi"; 
+        boolean checkStatus = true; 
+
+        System.out.println("Bill for patient " + patientId + " is Rs. 500");
+
+        System.out.println("Processing payment of Rs. 500 via " + upiId + "...");
+        System.out.println("Payment Successful. Transaction ID: TXN123456");
+
+        if (checkStatus) {
+            System.out.println("Status: Payment Completed");
+        }
+    }
+}
